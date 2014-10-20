048cfaf24ee7f6834d5ed67d1fa2caa336c0a005
Tom Howard <tom@windyroad.com.au>
2014-10-21 00:29:47 +1100

    Fixed validation and Added ScreenShots

--- src/test/java/au/com/windyroad/salesleads/ui/Driver.java
+++ src/test/java/au/com/windyroad/salesleads/ui/Driver.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -57,4 +59,8 @@ public class Driver implements Serializable {
 		return driver;
 	}
 
+	public byte[] getPngScreenShot() {
+		return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+	}
+	
 }
